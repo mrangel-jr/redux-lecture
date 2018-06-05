106cd36d364703b6b393c875abdbf83f2ef9a463
mrangel-jr <marcos@censanet.com.br>
2018-06-04 23:47:50 -0300

    Create Erro404
    Add new component Erro404 to adjust when i can't locate more a post or comment

---
+++ frontend/src/components/Erro404.js
@@ -0,0 +1,11 @@
+import React, {Component| from 'react';
+
+class Erro404 extends Component {
+    render() {
+        <div className="body">
+            Erro 404
+        </div>
+    }
+}
+
+export default Erro404;
