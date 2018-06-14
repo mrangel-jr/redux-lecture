9bfde752675b31ffd0274f9927df1f0a292f143a
mrangel-jr <marcos@censanet.com.br>
2018-06-14 09:29:21 -0300

    Correcting render method
    Add "return"  in render method

--- frontend/src/components/Erro404.js
+++ frontend/src/components/Erro404.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 
 class Erro404 extends Component {
     render() {
-        <div className="body">
-            Erro 404
-        </div>
+        return (
+            <div className="body">
+                Erro 404
+            </div>
+        );
     }
 }
 
